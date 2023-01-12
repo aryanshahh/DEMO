db594d20bcdc60a4c8a2951c5c5e055b6436c38f
aryanshahh <115704180+aryanshahh@users.noreply.github.com>
2023-01-12 10:25:08 +0530

    Delete DEMO directory

--- DEMO/src/com/demo/brake.java
+++
@@ -1,5 +0,0 @@
-package com.demo;
- public interface brake {
-        public void apply();
-        public void release();
-    }
--- DEMO/src/com/demo/engine.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-
-public interface engine {
-    public void start();
-    public void stop();
-}
